d8f18045475bc18b429275db70a79e99896e7fdb
Josh Herald Abelardo <joshheraldabelardo@Ikyangs-MacBook-Pro.local>
2025-09-04 11:32:28 +0800

    Day 2 of practicing node projects

--- project/bin/fs.js
+++ project/bin/fs.js
@@ -7,8 +7,8 @@ const secondPath = join(__dirname, '../', 'utils/', 'second.txt');
 const firstFile = readFileSync(firstPath, 'utf8');
 const secondFile = readFileSync(secondPath, 'utf8');
 
-var name = "Brylle";
+var name = "Josh";
 
 writeFileSync(firstPath, `Brylle has deeznuts`);
 writeFileSync(secondPath, `Brylle has deeznuts`);
-writeFileSync(join(__dirname, '../', 'utils/', 'sync_file.txt'), `Can ${name} itterate through deeznuts`);
+writeFileSync(join(__dirname, '../', 'utils/', 'sync_file.txt'), `Can ${name} itterate through deeznuts\n`, {flag : 'a'});
